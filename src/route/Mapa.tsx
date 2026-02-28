4108c2541f88ccba6a9652e29a5d8df870c416da
thaylanbf1 <thaylanfonseca12@gmail.com>
2026-02-28 20:03:42 -0300

    Mudança de estilo

--- src/route/Mapa.tsx
+++ src/route/Mapa.tsx
@@ -2,8 +2,11 @@
 const Mapa = () => {
   return (
     <section className='flex items-center justify-center px-6 py-16'>
-      <div className='max-w-5xl w-full flex flex-col md:flex-row items-center gap-8'>
-            <div className='bg-[#c5e7f04b] rounded-2xl shadow-sm p-10 flex flex-col gap-6 md:w-2/5 w-full'>
+      <div className='bg-[#c5e7f04b] max-w-5xl w-full rounded-2xl flex flex-col md:flex-row items-center gap-8'>
+            <div className='md:w-3/5 w-full rounded-2xl overflow-hidden shadow-md'>
+                <iframe src="https://www.google.com/maps/d/embed?mid=14r7iFNNFtXVNN5wz3TTP6BVvcBwgu-I" width="100%" height="400" className='border-0 w-full' allowFullScreen loading='lazy'/>
+            </div>
+            <div className=' p-10 flex flex-col gap-6 md:w-2/5 w-full'>
                 <div>
                     <h2 className='text-4xl font-light'>Navegue</h2>
                     <h2 className='text-4xl font-bold text-[#03297a]'>pelo MAPA</h2>
@@ -23,9 +26,9 @@ const Mapa = () => {
                     </li>
                 </ol>
             </div>
-                <div className='md:w-3/5 w-full rounded-2xl overflow-hidden shadow-md'>
+                {/* <div className='md:w-3/5 w-full rounded-2xl overflow-hidden shadow-md'>
                     <iframe src="https://www.google.com/maps/d/embed?mid=14r7iFNNFtXVNN5wz3TTP6BVvcBwgu-I" width="100%" height="400" className='border-0 w-full' allowFullScreen loading='lazy'/>
-                </div>
+                </div> */}
         </div>
     </section>
   )
